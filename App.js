235f0edc0828dce42c41a3d448424196191cebd6
Owen Xue <owensit98@gmail.com>
2022-01-14 12:10:05 -0500

    added header.js

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/header";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+  return (
+    <View style={styles.container}>
+      {/* header */}
+      <Header />
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
